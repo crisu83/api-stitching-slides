f7d12ce63d1815d5be36a764f588e7eaabba29fd
Christoffer Niska <christofferniska@gmail.com>
2018-05-18 10:29:58 +0200

    Update prezi

--- src/presentation.js
+++ src/presentation.js
@@ -3,15 +3,13 @@ import React from 'react';
 
 // Import Spectacle Core tags
 import {
-  // BlockQuote,
-  // Cite,
+  Code,
   Deck,
+  Fill,
   Heading,
   Image,
+  Layout,
   Link,
-  ListItem,
-  List,
-  // Quote,
   Slide,
   Text,
 } from 'spectacle';
@@ -25,7 +23,7 @@ require('normalize.css');
 const theme = createTheme(
   {
     primary: '#FFF',
-    secondary: '#001F3F',
+    secondary: '#222',
     tertiary: '#F012BE',
     quartenary: '#DDD',
   },
@@ -43,105 +41,93 @@ export default class Presentation extends React.Component {
         transitionDuration={500}
         theme={theme}
       >
-        <Slide transition={['zoom']} bgColor="primary">
-          <Heading size={3}>
+        <Slide transition={['fade']} bgColor="secondary">
+          <Heading size={3} textColor="primary" style={{margin: '0 20px'}}>
             GraphQL API stitching with Apollo
           </Heading>
-          <Text>
-            Combine your APIs into one
-          </Text>
         </Slide>
-        <Slide transition={['fade']} bgColor="primary">
-          <Text style={{fontWeight: 'bold'}}>
-            Christoffer Niska
-          </Text>
-          <Text margin="20px 0">
-            Head of Technology, <br/>
-            Digital Services at Digia
-          </Text>
-          <Link href="https://github.com/crisu83/">github.com/crisu83</Link><br/>
-          <Link href="https://twitter.com/crisu83/">twitter.com/crisu83</Link><br/>
-          <Link href="https://about.me/crisu83/">about.me/crisu83</Link>
-        </Slide>
-        <Slide transition={['fade']} bgColor="primary">
-          <Text>
-            GraphQL is an API query language
-          </Text>
-        </Slide>
-        <Slide transition={['fade']} bgColor="primary">
-          <Text>
-            Apollo is a GraphQL client
-          </Text>
-        </Slide>
-        <Slide transition={['fade']} bgColor="primary">
-          <Heading size={3}>
-            Problem
+        <Slide transition={['fade']} bgColor="secondary">
+          <Heading size={3} textColor="primary">
+            Hi, I'm Christoffer
           </Heading>
-          <Text>
-            How to query multiple APIs from a single endpoint?
+        </Slide>
+        <Slide transition={['fade']} bgColor="secondary">
+          <Text textColor="primary">
+            GraphQL is an API query language.<br/>
+            Apollo is a GraphQL client.
           </Text>
         </Slide>
-        <Slide transition={['fade']} bgColor="primary">
-          <Image src={require('./images/1-thread-type.png')} alt="" />
-          <Image src={require('./images/2-user-type.png')} alt="" />
-          <Image src={require('./images/3-thread-query.png')} alt="" />
-        </Slide>
-        <Slide transition={['fade']} bgColor="primary">
-          <Text>
-            Schema stitching allow combining multiple GraphQL APIs into a single GraphQL API.
+        <Slide transition={['fade']} bgColor="secondary">
+          <Text textColor="primary" style={{margin: '0 80px'}}>
+            How to combine multiple GraphQL APIs to form a single API?
           </Text>
         </Slide>
-        <Slide transition={['fade']} bgColor="primary">
-          <Text>
+        <Slide transition={['fade']} bgColor="secondary">
+          <Layout>
+            <Fill>
+              <Image src={require('./images/1-thread-type.png')} />
+            </Fill>
+            <Fill>
+            <Image src={require('./images/2-user-type.png')} />
+            </Fill>
+          </Layout>
+        </Slide>
+        <Slide transition={['fade']} bgColor="secondary">
+          <Image src={require('./images/3-thread-query.png')} />
+        </Slide>
+        <Slide transition={['fade']} bgColor="secondary">
+          <Heading size={3} textColor="primary">
+            Schema stitching
+          </Heading>
+        </Slide>
+        <Slide transition={['fade']} bgColor="secondary">
+          <Text textColor="primary">
             Links are fields that combines two types from different schemas.
           </Text>
           <Image src={require('./images/4-extend-thread-type.png')} alt="" />
         </Slide>
-        <Slide transition={['fade']} bgColor="primary">
-          <Text>
-            Schema transforms is a new concept that solves many issues related to combining of schemas.
+        <Slide transition={['fade']} bgColor="secondary">
+          <Text textColor="primary">
+            <Code textColor="primary" textSize={60}>
+              graphql-tools@3
+            </Code>
           </Text>
         </Slide>
-        <Slide transition={['fade']} bgColor="primary">
-          <Text>
-            Filter transform allow selecting the entry points for a schema, as well as removing certain types from the schema.
+        <Slide transition={['fade']} bgColor="secondary">
+          <Text textColor="primary" style={{margin: '0 80px'}}>
+            <b>Schema transforms</b> is a new concept that solves many issues related to merging schemas.
+          </Text>
+        </Slide>
+        <Slide transition={['fade']} bgColor="secondary">
+          <Text textColor="primary" style={{margin: '0 80px'}}>
+            <b>Filter transform</b> allow selecting the entry points for a schema, as well as removing certain types from the schema.
           </Text>
           <Image src={require('./images/5-filter-transform.png')} alt="" />
         </Slide>
-        <Slide transition={['fade']} bgColor="primary">
-          <Text>
-            Rename transforms allow renaming parts of a schema, which is especially useful for conflicts between type names.
+        <Slide transition={['fade']} bgColor="secondary">
+          <Text textColor="primary" style={{margin: '0 80px'}}>
+            <b>Rename transforms</b> allow renaming parts of a schema, which is especially useful for resolving conflicts between type names.
           </Text>
           <Image src={require('./images/6-rename-transforms.png')} alt="" />
         </Slide>
-        <Slide transition={['fade']} bgColor="primary">
-          <Text>
+        <Slide transition={['fade']} bgColor="secondary">
+          <Text textColor="primary" style={{margin: '0 80px'}}>
             You can also create your own custom transforms to customize the result of combining your schemas.
           </Text>
         </Slide>
-        <Slide transition={['fade']} bgColor="primary">
-          <Text>
-            Schema delegation allow keeping the original, untransformed schema around.
+        <Slide transition={['fade']} bgColor="secondary">
+          <Text textColor="primary">
+            <b>Schema delegation</b> allow keeping the original, untransformed schema around.
           </Text>
           <Image src={require('./images/7-schema-delegation.png')} alt="" />
         </Slide>
-        <Slide transition={['fade']} bgColor="primary">
-          <Heading size={3}>
-            Fin.
+        <Slide transition={['fade']} bgColor="secondary">
+          <Heading size={3} textColor="primary" style={{margin: '0 0 40px'}}>
+            Thank you!
           </Heading>
           <Text>
-            Questions?
+            <Link href="https://twitter.com/crisu83/" textColor="primary">@Crisu83</Link>
           </Text>
-        </Slide>
-        <Slide transition={['fade']} bgColor="primary">
-          <Heading size={3}>
-            Resources
-          </Heading>
-          <List>
-            <ListItem><Link href="https://dev-blog.apollodata.com/graphql-schema-stitching-8af23354ac37" target="_blank">GraphQL schema stitching</Link></ListItem>
-            <ListItem><Link href="https://dev-blog.apollodata.com/the-next-generation-of-schema-stitching-2716b3b259c0" target="_blank">The next generation of schema stitching</Link></ListItem>
-            <ListItem><Link href="https://www.apollographql.com/docs/graphql-tools/schema-stitching.html" target="_blank">Schema stitching docs</Link></ListItem>
-          </List>
         </Slide>
       </Deck>
     );
